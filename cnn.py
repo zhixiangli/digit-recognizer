b72d553db9d52231a6a0c7a35f4b24f2a7a1153d
Zhixiang Li <lizx@seagroup.com>
2017-10-28 14:05:25 +0800

    update cnn model

--- cnn.py
+++ cnn.py
@@ -1,54 +1,81 @@
+import argparse
+
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas
-import numpy
+from keras.layers import Dense, Dropout, Flatten, Conv2D, BatchNormalization
+from keras.layers.convolutional import MaxPooling2D
 from keras.models import Sequential
-from keras.layers.core import Dropout, Dense, Activation, Flatten
-from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.optimizers import Adam
+from keras.preprocessing.image import ImageDataGenerator
 from keras.utils import np_utils
 
-
-numpy.random.seed(1337) # for reproducibility
+np.random.seed(1337)  # for reproducibility
 images_size = 28
 
 
-model = Sequential()
-
-model.add(Convolution2D(32, 3, 3, input_shape=(1, images_size, images_size), dim_ordering='th'))
-model.add(Activation('relu'))
-model.add(Convolution2D(32, 3, 3, dim_ordering='th'))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Convolution2D(64, 3, 3, dim_ordering='th'))
-model.add(Activation('relu'))
-model.add(Convolution2D(64, 3, 3, dim_ordering='th'))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Flatten())
-model.add(Dense(128, init='normal'))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-
-model.add(Dense(10, init='normal'))
-model.add(Activation('softmax'))
-
-model.compile(loss='categorical_crossentropy', optimizer='adadelta')
+def load_train_data(train_csv):
+    train = pandas.read_csv(train_csv)
+    train_x = train.values[:, 1:].astype("float32")
+    train_x = train_x.reshape(train_x.shape[0], images_size, images_size, 1)
+    train_x = train_x / 255.0
+    train_y = np_utils.to_categorical(train.ix[:, 0].values.astype("int8"))
+    return train_x, train_y
 
 
-train = pandas.read_csv('./train.csv')
-train_x = train.values[:, 1:].astype("float32")
-train_x = train_x.reshape(train_x.shape[0], 1, 28, 28)
-train_x = train_x / 255.0
-train_y = np_utils.to_categorical(train.ix[:, 0].values.astype("int8"))
+def build_model():
+    model = Sequential()
 
-test = pandas.read_csv('./test.csv')
-test_x = test.values.astype("float32")
-test_x = test_x.reshape(test_x.shape[0], 1, 28, 28)
-test_x = test_x / 255.0
+    model.add(Conv2D(16, (3, 3), input_shape=(images_size, images_size, 1), activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Conv2D(16, (3, 3), input_shape=(images_size, images_size, 1), activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(32, (3, 3), input_shape=(images_size, images_size, 1), activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Conv2D(32, (3, 3), input_shape=(images_size, images_size, 1), activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.25))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.25))
+    model.add(Dense(10, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer=Adam(), metrics=["accuracy"])
+    return model
 
 
-model.fit(train_x, train_y, nb_epoch=32, batch_size=32, verbose=1, shuffle=True)
+def train_model(model, train_x, train_y):
+    data_gen = ImageDataGenerator(zoom_range=0.2, height_shift_range=0.2, width_shift_range=0.2, rotation_range=20)
+    hist = model.fit_generator(data_gen.flow(train_x, train_y), steps_per_epoch=1000, epochs=100, verbose=1)
+    plt.plot(hist.history['loss'], color='b')
+    plt.plot(hist.history['acc'], color='r')
+    plt.show()
 
 
-test_y = model.predict_classes(test_x)
-pandas.DataFrame({"ImageId": range(1, len(test_y) + 1), "Label": test_y}).to_csv('./cnn.csv', index=False, header=True)
+def predict(model, test_csv, result_csv):
+    test = pandas.read_csv(test_csv)
+    test_x = test.values.astype("float32")
+    test_x = test_x.reshape(test_x.shape[0], images_size, images_size, 1)
+    test_x = test_x / 255.0
+    test_y = model.predict_classes(test_x)
+    pandas.DataFrame({"ImageId": range(1, len(test_y) + 1), "Label": test_y}).to_csv(result_csv, index=False,
+                                                                                     header=True)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('train_data', action="store", default="./train.csv")
+    parser.add_argument('test_data', action="store", default="./test.csv")
+    parser.add_argument('predict_data', action="store", default="./result.csv")
+    args = parser.parse_args()
+
+    train_x, train_y = load_train_data(args.train_data)
+    model = build_model()
+    train_model(model, train_x, train_y)
+    predict(model, args.test_data, args.predict_data)
